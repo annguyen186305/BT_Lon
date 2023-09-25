ca91026c7d8dcbf5413a5eb3c2821123b2ceb74c
annguyen186305 <annguyen186305@gmail.com>
2023-09-25 16:19:24 +0700

    update

--- src/PhieuMuon.java
+++ src/PhieuMuon.java
@@ -1,11 +1,46 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class PhieuMuon {
+public class PhieuMuon  {
     private int soPhieu;
     private String ngayMuon;
     private String ngayHenTra;
     ArrayList<Sach> lstSach;
-    private ThuThu thuthu;
-    private BanDoc bandoc;
-    public void nhap(){}
+    private ThuThu tt ;
+    private  BanDoc bd;
+
+    public void nhap(){
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Nhap vao so phieu : ");
+        soPhieu= sc.nextInt();
+        System.out.print("Nhap thong tin ban doc ");
+        bd = new BanDoc();
+        bd.nhap();
+        System.out.print("Nhap thong tin thu thu");
+        tt = new ThuThu();
+        tt.nhap();
+        int n;
+        System.out.print("Nhap so sach muon:");
+        n = sc.nextInt();
+        lstSach = new ArrayList<Sach>(n);
+        for(int i=0; i<n; i++) {
+            Sach sach = new Sach();
+            sach.nhap();
+            lstSach.add(sach);
+        }
+        System.out.println("Nhap vao ngay muon: ");
+        ngayMuon= sc.nextLine();
+        System.out.println("Nhap vao ngay hen tra : ");
+        ngayHenTra= sc.nextLine();
+    }
+    public void xuat(){
+        System.out.print("\n so phieu" + soPhieu);
+        bd.xuat();
+        System.out.print("\n danh sach sach muon");
+        for( Sach sach: lstSach)
+            sach.xuat();
+        tt.xuat();
+        System.out.print("\n ngay muon" + ngayMuon);
+        System.out.print("\n ngay tra" + ngayHenTra);
+    }
 }
---
+++ src/dsPhieuMuon.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class dsPhieuMuon {
+    ArrayList <PhieuMuon> lstPM;
+    public void nhapdsPhieuMuon(){
+        int soPM;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao so phieu muon:");
+        soPM = sc.nextInt();
+        lstPM = new ArrayList<PhieuMuon>(soPM);
+        for (int i=0; i<soPM; i++){
+            PhieuMuon x = new PhieuMuon();
+            x.nhap();
+            lstPM.add(x);
+        }
+    }
+    public void xuatdsPhieuMuon(){
+        for (PhieuMuon PM: lstPM)
+            PM.xuat();
+    }
+}
