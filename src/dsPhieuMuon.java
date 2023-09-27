00925ca4bd5fbfe5c051ffae9f03e3e10a250568
annguyen186305 <annguyen186305@gmail.com>
2023-09-27 11:29:42 +0700

    update

--- src/dsPhieuMuon.java
+++ src/dsPhieuMuon.java
@@ -2,30 +2,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class dsPhieuMuon {
-    ArrayList <PhieuMuon> lstPM;
-    public void nhapdsPhieuMuon(){
+    ArrayList<PhieuMuon> lstPM;
+
+    public void nhapdsPhieuMuon() {
         int soPM;
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap vao tong so phieu muon:");
         soPM = sc.nextInt();
         sc.nextLine();
         lstPM = new ArrayList<PhieuMuon>(soPM);
-        for (int i=0; i<soPM; i++){
+        for (int i = 0; i < soPM; i++) {
             PhieuMuon x = new PhieuMuon();
             x.nhap();
             lstPM.add(x);
         }
     }
-    public void xuatdsPhieuMuon(){
-        for (PhieuMuon PM: lstPM)
+
+    public void xuatdsPhieuMuon() {
+        for (PhieuMuon PM : lstPM)
             PM.xuat();
     }
+
     public void timKiemPhieuMuonTheoMaSach(String maSach) {
         boolean timThay = false;
         for (PhieuMuon PM : lstPM) {
-            if (PM.timSach(maSach)!=-1)
-            {
-                PM.xuat();
+            int viTriSach = PM.timSach(maSach);
+            if (viTriSach != -1) {
+                Sach sachDaMuon = PM.lstSach.get(viTriSach);
+                sachDaMuon.xuat();
+                BanDoc banDoc = timBanDocTheoMaSach(maSach);
+                if (banDoc != null) {
+                    banDoc.xuat();
+                } else {
+                    System.out.println("Khong tim thay thong tin ban doc.");
+                }
                 timThay = true;
             }
         }
@@ -33,4 +43,15 @@ public class dsPhieuMuon {
             System.out.println("Khong tim thay phieu muon nao cho sach co ma: " + maSach);
         }
     }
+
+    public BanDoc timBanDocTheoMaSach(String maSach) {
+        for (PhieuMuon phieuMuon : lstPM) {
+            int viTriSach = phieuMuon.timSach(maSach);
+            if (viTriSach != -1) {
+                return phieuMuon.getBd();
+            }
+        }
+        return null;
+    }
 }
+
